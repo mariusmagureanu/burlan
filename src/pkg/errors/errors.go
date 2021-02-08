73149a648d7ef7bc4ab17180057ad1b30084d094
Marius Magureanu <mariusmagureanu@live.dk>
2021-02-08 10:53:47 +0100

    Added new jwt errors

--- src/pkg/errors/errors.go
+++ src/pkg/errors/errors.go
@@ -4,4 +4,6 @@ import "errors"
 
 var (
 	ErrWSInvalidFormat = errors.New("invalid web socket message format ")
+	ErrCannotParseClaims = errors.New("cannot parse claims")
+	ErrJWTIsExpired = errors.New("jwt is expired")
 )
